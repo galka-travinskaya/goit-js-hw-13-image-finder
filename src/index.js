9893db82ed1a4b74e77a931f5c55b1b5ae9979c8
galka-travinskaya <galka03031988@rambler.ru>
2020-11-22 14:26:35 +0200

    hw

--- src/index.js
+++ src/index.js
@@ -1,73 +1,99 @@
 import './css/styles.css';
-import imageListTpl from './templates/imageCard.hbs';
-import HitsApiService from './js/Api/api-service';
+import imageListTpl from './templates/imageList.hbs';
+import HitsApiService from './js/Api/apiService';
+import infiniteScroll from 'infinite-scroll';
 import LoadMoreBtn from './js/loadMoreBtn';
-import { alert, defaultModules } from '@pnotify/core';
-import * as PNotifyMobile from '@pnotify/mobile';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
 
-defaultModules.set(PNotifyMobile, {});
-
-alert({
-  text: 'Notice me, senpai!'
-});
+var debounce = require('lodash.debounce');
 
 const refs ={
     searchForm: document.querySelector('.search-form'),
-    // loadMoreBtn: document.querySelector('[data-action="load-more"]'),
     galleryList: document.querySelector('.gallery'),
+    input: document.querySelector('.search-input'),
+    sentinel: document.querySelector('#sentinel'),
 };
 
 const loadMoreBtn = new LoadMoreBtn({
     selector: '[data-action="load-more"]',
+    hidden: true,
 });
-const hitsApiService = new HitsApiService();
-console.log(loadMoreBtn);
 
-refs.searchForm.addEventListener('submit', onSearch);
-// refs.loadMoreBtn.addEventListener('click', onLoadMore);
-loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
-
+refs.input.addEventListener('input', debounce(onSearch, 500));
+// loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
+refs.galleryList.addEventListener('click', onGalleryElClick); 
 
 function onSearch(e) {
     e.preventDefault();
 
-    hitsApiService.query = e.currentTarget.elements.query.value;
-    
-    if(hitsApiService.query === '') {
-        return alert('Введите корректное слово');
+    hitsApiService.query = e.target.value.trim();
+    if(hitsApiService.query.length === 0) {
+        return clearHitsContainer();
     }
-
+    // Прячем кнопку
+    // loadMoreBtn.show();
     hitsApiService.resetPage();
-    hitsApiService.fetchHits().then(hits => {
-        calearHitsContainer();
-        addHitsMarkup(hits);
-    });
+    clearHitsContainer();
+    onLoadMore(); 
 }
 
-function onLoadMore(e) {
-    hitsApiService.fetchHits().then(addHitsMarkup);
+function onLoadMore() {
+    loadMoreBtn.disable();
+    hitsApiService.fetchHits().then(hits => {
+        addHitsMarkup(hits);
+        // scrollPage();
+        loadMoreBtn.enable();
+    });
 }
 
 function addHitsMarkup(hits) {
     refs.galleryList.insertAdjacentHTML('beforeend', imageListTpl(hits));
 }
 
-function calearHitsContainer() {
+function clearHitsContainer() {
  refs.galleryList.innerHTML = '';    
 }
 
-// Саши скрол 1
-// function scrollTo(st) {
-//     window.scrollTo(0,st - 40)
-//   }
-//   function onLoadMore(e) {
-//     const st = window.pageYOffset + window.innerHeight;
-//     if (!imagesApiServer.query) return;
-  
-//     imagesApiServer.incrementPage();
-//     imagesApiServer.fetchGallery()
-//       .then(renderImagesGallery)
-//       .then(() => scrollTo(st))
-//     .catch(error => console.log(error));
-//   }
-  
+// function scrollPage() {
+//       try {
+//         setTimeout(() => {
+//           window.scrollTo({
+//             top: document.body.scrollHeight,
+//             left: 0,
+//             behavior: 'smooth',
+//           });
+//         }, 1000);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     }
+
+    const hitsApiService = new HitsApiService();
+
+    const onEntry = entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting && hitsApiService.query !== '') {
+                console.log("LOADING...");
+                hitsApiService.fetchHits().then(hits => {
+                    addHitsMarkup(hits);
+                    hitsApiService.incrementPage();
+                });
+            }
+        });
+    }
+    const options = {
+        rootMargin: '100px'
+    }
+    const observer = new IntersectionObserver(onEntry, options);
+    observer.observe(refs.sentinel);
+
+function onGalleryElClick(event) {
+        if (event.target.nodeName !== 'IMG') {
+          return;
+        }
+        
+        const changeModalImage = `<img src=${event.target.dataset.source} alt="icon" />`;
+        const instance = basicLightbox.create(changeModalImage);
+        instance.show();
+ }
