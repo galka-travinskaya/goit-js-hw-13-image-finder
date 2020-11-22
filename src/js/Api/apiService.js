e8b0bc755b9f6fcfd20c2b69b36559fc5d83c6e3
galka-travinskaya <galka03031988@rambler.ru>
2020-11-22 15:02:15 +0200

    test

---
+++ src/js/Api/apiService.js
@@ -0,0 +1,38 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '19136435-96f0ae3906c94a349fe1f1440';
+
+export default class apiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    fetchHits() {
+        const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&per_page=12&page=${this.page}`;
+
+        return fetch(url)
+            .then(response => response.json())
+            .then(({hits}) => {
+                this.incrementPage();
+
+                return hits;
+            });
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    // Если убрать get и set, тогда везьде обращаемся только к searchQuery, a не query 
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
